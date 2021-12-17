2faa0c909be1ea7b8ed6c04b0bff745accfcfa79
rmelchorv <rmelchorv@gmail.com>
2021-12-17 09:06:49 -0600

    Componente Peliculas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MyFirst } from './my-first/my-first.component';
+import { MyFirst } from './components/my-first/my-first.component';
+import { FilmsComponent } from './components/films/films.component';
 
 @NgModule({
   declarations: [
-    AppComponent,MyFirst
+    AppComponent, MyFirst, FilmsComponent
   ],
   imports: [
     BrowserModule
