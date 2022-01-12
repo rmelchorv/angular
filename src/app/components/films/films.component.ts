2372b188c374b0c7034d14bc019eb1ef2965bca3
rmelchorv <rmelchorv@gmail.com>
2022-01-12 11:13:27 -0600

    *ngIf, *ngFor

--- src/app/components/films/films.component.ts
+++ src/app/components/films/films.component.ts
@@ -7,16 +7,29 @@ import { Film  } from 'src/app/models/films';
 })
 export class FilmsComponent implements OnInit {
   public films : Array<Film>;
+  public genres : Array<string>;
+  public showFilms : boolean;
 
   constructor() { 
     this.films = [
-      new Film("Dora","Animation",2000,"James Bobby"),
-      new Film("Jeepers Creepers","Terror",2001,"Victor Salva"),
-      new Film("Revolver","Crimen",2005,"Unkown")
+      new Film("Dora and the Lost City of Gold","Adventure",2019,"James Bobin",1,"EE.UU."),
+      new Film("Jeepers Creepers","Terror",2001,"Victor Salva",1,"EE.UU."),
+      new Film("Revolver","Crime",2005,"Guy Ritchie",1,"U.K."),
+      new Film("Child's Play","Terror",1988,"Tom Holland",0,"EE.UU.")
     ];
+    this.genres = [];
+    this.showFilms = true;
   }
 
   ngOnInit(): void {
+    this.getGenres();
+  }
+
+  getGenres() {
+    this.films.forEach((film) => {
+      if(this.genres.indexOf(film.genre)<0)
+        this.genres.push(film.genre);
+    });
   }
 
 }
